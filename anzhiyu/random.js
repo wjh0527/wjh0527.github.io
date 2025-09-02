ce76858f84652bad057fb22da72700446a038ccc
王家辉 <15158302+wang-jiahui0527@user.noreply.gitee.com>
2025-09-02 17:57:43 +0800

    Site updated: 2025-09-02 17:57:42

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/09/02/hello-world/","2025/08/30/web/"];function toRandomPost(){
+var posts=["2025/08/30/web/","2025/09/02/hello-world/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
