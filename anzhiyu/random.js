f264de6a527c63d4d8a4f5c06fd8403c5ac92cd6
王家辉 <15158302+wang-jiahui0527@user.noreply.gitee.com>
2025-09-03 10:20:34 +0800

    Site updated: 2025-09-03 10:20:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/08/30/web/","2025/09/02/hello-world/"];function toRandomPost(){
+var posts=["2025/09/02/hello-world/","2025/08/30/web/","2025/09/03/SQL注入/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
